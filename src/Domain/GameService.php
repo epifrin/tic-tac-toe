0246f79eb83e9f57533f3c740f16b4251a4c0ac6
epifrin <epifrin@i.ua>
2022-10-27 21:55:04 +0300

    add method makeComputerMove

--- src/Domain/GameService.php
+++ src/Domain/GameService.php
@@ -6,13 +6,16 @@ namespace App\Domain;
 
 use App\Domain\Board\Board;
 use App\Domain\Cell\CellType;
+use App\Domain\Computer\Strategy\StrategyInterface;
 use App\Domain\GameResult\GameResult;
 use App\Domain\GameResult\GameResultChecker;
 
 class GameService
 {
-    public function __construct(private Board $board)
-    {
+    public function __construct(
+        private readonly Board $board,
+        private readonly StrategyInterface $strategy
+    ) {
     }
 
     public function handleMove(CellType $cellType, int $place): Board
@@ -24,5 +27,11 @@ class GameService
     public function checkResult(): GameResult
     {
         return (new GameResultChecker())->check($this->board);
+    }
+
+    public function makeComputerMove(): Board
+    {
+        $place = $this->strategy->getComputerMove($this->board);
+        return $this->handleMove(CellType::O, $place);
     }
 }
